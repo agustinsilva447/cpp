6f9deeb48ffdaa9e5832bd7e8f3aaeeab996a0d2
agustin <agustinsilva447@gmail.com>
2022-04-14 23:44:42 -0300

    depth of tree

--- fb/test_2.py
+++ fb/test_2.py
@@ -1,51 +1,33 @@
-import collections
+class TreeNode: 
+  def __init__(self,key): 
+    self.left = None
+    self.right = None
+    self.val = key 
 
-class Node:
-    def __init__(self, val=None):
-        self.left = None
-        self.right = None
-        self.val =  val       
+def depth_nodes(root):
+  if root == None:
+    return 0
+  else:
+    vis_left  = depth_nodes(root.left)
+    vis_right = depth_nodes(root.right)
+    
+    if vis_left > vis_right:
+      return vis_left + 1
+    else:
+      return vis_right + 1 
 
-def levelOrderPrint(tree):
-    if not tree:
-        return
-    nodes = collections.deque([tree])
-    currentCount, nextCount = 1, 0
-    while (len(nodes)!=0):
-        currentNode = nodes.popleft()
-        currentCount -= 1
-        print(currentNode.val,end=" ")
-        if currentNode.left:
-            nodes.append(currentNode.left)
-            nextCount += 1
-        if currentNode.right:
-            nodes.append(currentNode.right)
-            nextCount += 1
-        if (currentCount == 0):
-            print("\n")
-            currentCount, nextCount = nextCount, currentCount
+def visible_nodes(root):  
+  return depth_nodes(root)
 
-def trimBST(tree, minVal, maxVal):     
-    if not tree: 
-        return     
-    tree.left  = trimBST(tree.left,  minVal, maxVal) 
-    tree.right = trimBST(tree.right, minVal, maxVal)     
-    if (minVal<=tree.val<=maxVal): 
-        return tree     
-    if (tree.val<minVal): 
-        return tree.right     
-    if (tree.val>maxVal): 
-        return tree.left 
+root_1 = TreeNode(8)
+root_1.left = TreeNode(3)
+root_1.left.left = TreeNode(1)
+root_1.left.right = TreeNode(6)
+root_1.left.right.left = TreeNode(4)
+root_1.left.right.right = TreeNode(7)
+root_1.right = TreeNode(10)
+root_1.right.right = TreeNode(14)
+root_1.right.right.left = TreeNode(13)
 
-tree = Node(8)
-tree.left = Node(3)
-tree.left.left = Node(1)
-tree.left.right = Node(6)
-tree.left.right.left = Node(4)
-tree.left.right.right = Node(7)
-tree.right = Node(10)
-tree.right.right = Node(14)
-tree.right.right.left = Node(13)
-levelOrderPrint(tree)
-tree2 = trimBST(tree, 5, 13)
-levelOrderPrint(tree2)
+a = visible_nodes(root_1)
+print(a)
