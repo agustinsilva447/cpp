7d534bdc918a4820f474fb8d01efad2fafe77faa
agustin <agustinsilva447@gmail.com>
2022-04-19 13:53:14 -0300

    testing

--- fb/test_2.py
+++ fb/test_2.py
@@ -1,24 +1,28 @@
-def isLongPressedName(name, typed):
-    i = 0
-    j = 0
-    if name[0] != typed[0]:
-        return False
-    while j<len(typed):
-        print(i, len(name), j, len(typed))
-        if i<len(name):
-            if name[i] == typed[j]:
-                i += 1
-                j += 1
-            elif name[i-1] == typed[j]:
-                j += 1
-            else:
-                return False
-        else:
-            if typed[j-1] == typed[j]:
-                j += 1
-            else:
-                return False                    
-    return True
+import numpy as np
 
-a = isLongPressedName("ale", "alexd")
-print(a)
+def lengthOfLongestSubstring(s: str) -> int:
+    max_cnt = 1
+    left = 0
+    while left < len(s) - max_cnt:
+        right = left + max_cnt
+        while right < (len(s) + 1):
+            tot = set()
+            cnt = 0
+            for i in np.arange(left, right, 1):
+                if s[i] not in tot:
+                    tot.add(s[i])
+                    cnt += 1
+                else:
+                    if cnt>max_cnt:
+                        max_cnt = cnt
+                    tot = set()
+                    cnt = 0
+            if cnt>max_cnt:
+                max_cnt = cnt
+            right += 1
+        left += 1
+    return max_cnt
+
+a = "wllxdiklosdrdxfohgwringzefwbytmwgxtjhdxwycpbawphcnbmajmeokhoftlmsexakuyixplxmagoojdospvjbcxh"
+b = lengthOfLongestSubstring(a)
+print(a,b)
